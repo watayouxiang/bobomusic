dc1d9672be0e9efaf0db2b2c4b6d93128da8350f
Redstone-1 <1825744594@qq.com>
2025-04-23 20:22:48 +0800

    chore: 构建文件

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -4,7 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   media_kit_libs_linux
-  screen_retriever
+  screen_retriever_linux
   url_launcher_linux
   window_manager
 )
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,7 +5,7 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   media_kit_libs_windows_audio
   permission_handler_windows
-  screen_retriever
+  screen_retriever_windows
   url_launcher_windows
   window_manager
 )
