fc558b67228a5a6ed9f3fda834de9f3480950336
Redstone-1 <1825744594@qq.com>
2025-05-05 15:26:36 +0800

    version: 2.1.0

--- lib/modules/setting/setting.dart
+++ lib/modules/setting/setting.dart
@@ -192,7 +192,7 @@ class SettingViewState extends State<SettingView> with AutomaticKeepAliveClientM
                     backgroundImage: AssetImage("assets/ic_launch.png"),
                   ),
                   SizedBox(width: 6),
-                  Text("啵啵音乐v2.0.0", style: TextStyle(color: Colors.grey, fontSize: 12)),
+                  Text("啵啵音乐v2.1.0", style: TextStyle(color: Colors.grey, fontSize: 12)),
                 ],
               ),
               const SizedBox(height: 6),
